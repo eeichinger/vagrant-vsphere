5fd415f8b38715d51d44a802d86e2e2ed3af1992
Erich Eichinger <eeichinger@gmail.com>
2013-06-09 15:49:22 +0200

    working draft

--- lib/vSphere/action/clone.rb
+++ lib/vSphere/action/clone.rb
@@ -18,19 +18,34 @@ module VagrantPlugins
           dc = get_datacenter env[:vSphere_connection], env[:machine]
           template = dc.find_vm config.template_name
 
-          raise Error::VSphereError, :message => I18n.t('errors.missing_template') if template.nil?
+#          raise Errors::VSphereError, :message => I18n.t('errors.missing_template') if template.nil?
+          raise I18n.t('errors.missing_template') + ":" + config.template_name if template.nil?
 
           begin
             location = RbVmomi::VIM.VirtualMachineRelocateSpec :pool => get_resource_pool(env[:vSphere_connection], env[:machine])
-            spec = RbVmomi::VIM.VirtualMachineCloneSpec :location => location, :powerOn => true, :template => false
+
+			custName = RbVmomi::VIM.CustomizationVirtualMachineName
+			custLinPrep = RbVmomi::VIM.CustomizationLinuxPrep :hostName => custName, :domain => "local"
+
+			custGlobalIPSettings = RbVmomi::VIM.CustomizationGlobalIPSettings
+
+			custIP = RbVmomi::VIM.CustomizationDhcpIpGenerator
+			custIPSettings = RbVmomi::VIM.CustomizationIPSettings :ip => custIP #, :gateway, :subnetMask
+			custAdapterMapping = RbVmomi::VIM.CustomizationAdapterMapping :adapter => custIPSettings
+
+			cust_adapter_mapping_list = [custAdapterMapping]
+
+        	custSpec = RbVmomi::VIM.CustomizationSpec :identity => custLinPrep, :globalIPSettings => custGlobalIPSettings, :nicSettingMap => cust_adapter_mapping_list
+
+            spec = RbVmomi::VIM.VirtualMachineCloneSpec :location => location, :powerOn => true, :template => false, :customization => custSpec
 
             env[:ui].info I18n.t('vsphere.creating_cloned_vm')
             env[:ui].info " -- Template VM: #{config.template_name}"
             env[:ui].info " -- Name: #{config.name}"
 
             new_vm = template.CloneVM_Task(:folder => template.parent, :name => config.name, :spec => spec).wait_for_completion
-          rescue Exception => e
-            raise Errors::VSphereError, :message => e.message
+#          rescue Exception => e
+#            raise :messe.message
           end
 
           #TODO: handle interrupted status in the environment, should the vm be destroyed?
--- lib/vSphere/action/connect_vsphere.rb
+++ lib/vSphere/action/connect_vsphere.rb
@@ -12,10 +12,11 @@ module VagrantPlugins
           config = env[:machine].provider_config
 
           begin
+#            http.verify_mode = OpenSSL::SSL::VERIFY_NONE if http.use_ssl?
             env[:vSphere_connection] = RbVmomi::VIM.connect host: config.host, user: config.user, password: config.password, insecure: config.insecure
             @app.call env
-          rescue Exception => e
-            raise VagrantPlugins::VSphere::Errors::VSphereError, :message => e.message
+        #  rescue Exception => e
+        #   raise VagrantPlugins::VSphere::Errors::VSphereError, :message => e.message
           end
         end
       end
